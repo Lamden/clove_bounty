a67a41eec5331b537d5ece003b9f432622921aaa
Matt <matt@mattprimeonline.com>
2018-02-12 16:29:11 +0100

    Create experience_points.py

---
+++ network/experience_points.py
@@ -0,0 +1,24 @@
+from clove.network.bitcoin import Bitcoin
+
+
+class ExperiencePoints(Bitcoin):
+    """
+    Class with all the necessary Experience Points XP network information based on
+    https://github.com/eXperiencePoints/XPCoin/blob/master/src/net.cpp
+    (date of access: 02/12/2018)
+    """
+    name = 'experiencepoints'
+    symbols = ('XP', )
+    seeds = ('seed1.xpcoin.io', 'seed2.xpcoin.io', 'seed3.xpcoin.io', 'seed4.xpcoin.io')
+    port = 28192
+
+
+class ExperiencePointsTestNet(Adzcoin):
+    """
+    Class with all the necessary Experience Points XP testing network information based on
+    https://github.com/eXperiencePoints/XPCoin/blob/master/src/net.cpp    
+    (date of access: 02/12/2018)
+    """
+    name = 'test-experiencepoints'
+    seeds = ()
+    port = 17778
