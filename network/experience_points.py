2605e78c169b922ccca5b5aea662e719c830fb56
Stuart Farmer <stuart@lamden.io>
2018-02-12 09:52:30 -0800

    Update experience_points.py

--- network/experience_points.py
+++ network/experience_points.py
@@ -13,7 +13,7 @@ class ExperiencePoints(Bitcoin):
     port = 28192
 
 
-class ExperiencePointsTestNet(Adzcoin):
+class ExperiencePointsTestNet(ExperiencePoints):
     """
     Class with all the necessary Experience Points XP testing network information based on
     https://github.com/eXperiencePoints/XPCoin/blob/master/src/net.cpp    
