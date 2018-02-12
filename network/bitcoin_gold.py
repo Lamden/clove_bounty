7cdb55c41cd7a4c06510ccc189451816d88f4355
Matt <matt@mattprimeonline.com>
2018-02-12 13:32:45 +0100

    Create bitcoin_gold.py

---
+++ network/bitcoin_gold.py
@@ -0,0 +1,33 @@
+from clove.network.bitcoin import Bitcoin
+
+
+class BitcoinGold(Bitcoin):
+    """
+    Class with all the necessary BTG network information based on
+    https://github.com/BTCGPU/BTCGPU/blob/master/src/chainparams.cpp    
+    (date of access: 02/12/2018)
+    """
+    name = 'bitcoin-gold'
+    symbols = ('BTG', )
+    seeds = (
+        'eu-dnsseed.bitcoingold-official.org',
+        'dnsseed.bitcoingold.org',
+        'dnsseed.btcgpu.org',
+    )
+    port = 8338
+
+
+class BitcoinGoldTestNet(BitcoinGold):
+    """
+    Class with all the necessary BTG testing network information based on
+    https://github.com/BTCGPU/BTCGPU/blob/master/src/chainparams.cpp
+    (date of access: 02/12/2018)
+    """
+    name = 'test-bitcoin-gold'
+    seeds = (
+        'eu-test-dnsseed.bitcoingold-official.org',
+        'test-dnsseed.bitcoingold.org',
+        'test-dnsseed.btcgpu.org',
+        'btg.dnsseed.minertopia.org',        
+    )
+    port = 18338
