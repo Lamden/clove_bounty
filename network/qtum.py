1b04e64001ad5f6fec4560097f5a132051fd386a
Nam Ngo <namngo@nbnco.com.au>
2018-02-12 12:34:43 +1100

    Add QTUM (QTUM)

---
+++ network/qtum.py
@@ -0,0 +1,28 @@
+from clove.network.bitcoin import Bitcoin
+
+
+class Qtum(Bitcoin):
+    """
+    Class with all the necessary QTUM network information based on
+    https://github.com/qtumproject/qtum/blob/master/src/chainparams.cpp
+    (date of access: 01/18/2018)
+    """
+    name = 'qtum'
+    symbols = ('QTUM', )
+    seeds = (
+        'qtum3.dynu.net',
+    )
+    port = 3888
+
+
+class QtumTestNet(Qtum):
+    """
+    Class with all the necessary QTUM testing network information based on
+    https://github.com/qtumproject/qtum/blob/master/src/chainparams.cpp
+    (date of access: 01/18/2018)
+    """
+    name = 'test-qtum'
+    seeds = (
+        'qtum4.dynu.net',
+    )
+    port = 13888
