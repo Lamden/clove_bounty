4107daa07b86fadba553d4828d8bc6be5c6f2ce3
Nuno Mendes <nmendes@outlook.com>
2018-02-12 16:42:17 +0000

    Advanced Internet Blocks (AIB)

---
+++ network/advanced_internet_blocks.py
@@ -0,0 +1,25 @@
+
+from clove.network.bitcoin import Bitcoin
+
+
+class AdvancedInternetBlocks(Bitcoin):
+    """
+    Class with all the necessary AIB network information based on
+    https://github.com/iobond/aib/blob/master/src/chainparams.cpp
+    (date of access: 02/12/2018)
+    """
+    name = 'test'
+    symbols = ('AIB', )
+    seeds = ('seed.wtmint.com', 'seed.iobond.com', 'seed.aib.one',)
+    port = 5223
+
+
+class AdvancedInternetBlocksTestNet(AdvancedInternetBlocks):
+    """
+    Class with all the necessary AIB testing network information based on
+    https://github.com/iobond/aib/blob/master/src/chainparams.cpp
+    (date of access: 02/12/2018)
+    """
+    name = 'regtest'
+    seeds = (,)
+    port = 51423
