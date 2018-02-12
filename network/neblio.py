45c844858314673777658a606ddcd1d51b5dff05
Matt <matt@mattprimeonline.com>
2018-02-12 16:58:07 +0100

    Create neblio.py

---
+++ network/neblio.py
@@ -0,0 +1,24 @@
+from clove.network.bitcoin import Bitcoin
+
+
+class Neblio(Bitcoin):
+    """
+    Class with all the necessary Neblio (NEBL) network information based on
+    https://github.com/NeblioTeam/neblio/blob/master/src/net.cpp
+    (date of access: 02/12/2018)
+    """
+    name = 'neblio'
+    symbols = ('NEBL', )
+    seeds = ('seed.nebl.io')
+    port = 6325
+
+
+class NeblioTestNet(Adzcoin):
+    """
+    Class with all the necessary Neblio (NEBL) testing network information based on
+    https://github.com/NeblioTeam/neblio/blob/master/src/net.cpp
+    (date of access: 02/12/2018)
+    """
+    name = 'test-neblio'
+    seeds = ()
+    port = 16325
