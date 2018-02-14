5f5e4129770a4bafdd7e1527dd5b714732eb1a8f
Felix B <mail@bourier.biz>
2018-02-14 11:15:44 +0100

    Added Battlestake

---
+++ network/battlestake.py
@@ -0,0 +1,15 @@
+from clove.network.bitcoin import Bitcoin
+
+
+class BattleStake(Bitcoin):
+    """
+    Class with all the necessary BattleStake network information based on
+    https://github.com/battlestake/source/blob/master/src/net.cpp
+    (date of access: 02/14/2018)
+    """
+    name = 'BattleStake'
+    symbols = ('BSTK', )
+    seeds =  ("seed.battlestake.xyz")
+    port = 9542
+	
+# Has no testnet
