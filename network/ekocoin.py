b6442a351e7f3cfd6484e9ece75feeb0ea7fc3cb
Felix B <mail@bourier.biz>
2018-02-14 23:54:20 +0100

    Added 13 Altcoins

---
+++ network/ekocoin.py
@@ -0,0 +1,15 @@
+from clove.network.bitcoin import Bitcoin
+
+
+class EcoCoin(Bitcoin):
+    """
+    Class with all the necessary ECO network information based on
+    https://github.com/ekocoin/eko/blob/master/src/net.cpp
+    (date of access: 02/14/2018)
+    """
+    name = 'ecocoin'
+    symbols = ('ECO', )
+    seeds =  ("node.walletbuilders.com")
+    port = 7257
+	
+# no testnet
---
+++ network/equalcoin .py
@@ -0,0 +1,15 @@
+from clove.network.bitcoin import Bitcoin
+
+
+class EqualCoin(Bitcoin):
+    """
+    Class with all the necessary EqualCoin network information based on
+    https://github.com/equalcoinproject/equalcoin/blob/master/src/net.cpp
+    (date of access: 02/14/2018)
+    """
+    name = 'equalcoin'
+    symbols = ('EQUAL', )
+    seeds = ("216.31.12.38",
+             "216.31.12.99")
+    port = 13787
+	
