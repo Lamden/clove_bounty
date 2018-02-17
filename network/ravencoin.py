00c93bd42a2b120137253d45364a6216c724e489
Stuart Farmer <stuart@lamden.io>
2018-02-16 22:42:45 -0800

    KAWW!

---
+++ network/ravencoin.py
@@ -0,0 +1,30 @@
+from clove.network.bitcoin import Bitcoin
+
+
+class Ravencoin(Bitcoin):
+    """
+    Class with all the necessary LTC network information based on
+    https://github.com/RavenProject/Ravencoin/blob/master/src/chainparams.cpp
+    (date of access: 02/16/2018)
+    """
+    name = 'raven'
+    symbols = ('RVN', )
+    seeds = (
+        "seed-raven.ravencoin.org",
+        "seed-raven.bitactivate.com"
+    )
+    port = 8767
+
+
+class RavencoinTestNet(Ravencoin):
+    """
+    Class with all the necessary LTC testing network information based on
+    https://github.com/RavenProject/Ravencoin/blob/master/src/chainparams.cpp
+    (date of access: 02/16/2018)
+    """
+    name = 'test-raven'
+    seeds = (
+        "seed-testnet-raven.ravencoin.org",
+        "seed-testnet-raven.bitactivate.com"
+    )
+    port = 18767
