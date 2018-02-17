f8d09602610ee431ad8ce98f65eea065fbb27694
Ancil Cleetus <ancilcleetus@gmail.com>
2018-02-17 18:45:09 +0530

    Add files via upload

---
+++ network/agrolifecoin.py
@@ -0,0 +1,25 @@
+from clove.network.bitcoin import Bitcoin
+
+
+class  AgrolifeCoin(Bitcoin):
+    """
+    Class with all the necessary  AgrolifeCoin (AGLC) network information based on
+    https://github.com/traiborg/Agrolifecoin/blob/master/src/net.cpp
+    (date of access: 02/17/2018)
+    """
+    name = 'agrolifecoin'
+    symbols = ('AGLC', )
+    seeds =  ('seed.agrolifecoin.org', 'seed2.agrolifecoin.org')
+    port = 27330
+
+
+class  AgrolifeCoinTestNet(Aces):
+    """
+    Class with all the necessary  AgrolifeCoin (AGLC) network information based on
+    https://github.com/traiborg/Agrolifecoin/blob/master/src/net.cpp
+    (date of access: 02/17/2018)
+    """
+    name = 'test-agrolifecoin'
+    symbols = ('AGLC', )
+    seeds =  ()
+    port = 22332
