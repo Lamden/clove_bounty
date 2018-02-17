37c86eaa34eceeb381322adedab0b552c2e4a067
Ancil Cleetus <ancilcleetus@gmail.com>
2018-02-17 20:39:49 +0530

    Update b3coin.py

--- network/b3coin.py
+++ network/b3coin.py
@@ -9,7 +9,11 @@ class  B3Coin(Bitcoin):
     """
     name = 'b3coin'
     symbols = ('KB3', )
-    seeds =  ('seed001.b3nodes.net', 'seed002.b3nodes.net', 'seed003.b3nodes.net', 'seed004.b3nodes.net', 'seed005.b3nodes.net', 'seed006.b3nodes.net', 'seed007.b3nodes.net', 'seed008.b3nodes.net', 'seed008.b3nodes.net', 'seed010.b3nodes.net', 'seed011.b3nodes.net', 'seed012.b3nodes.net', 'seed013.b3nodes.net', 'seed014.b3nodes.net', 'seed015.b3nodes.net', 'seed016.b3nodes.net', 'seed017.b3nodes.net', 'seed018.b3nodes.net', 'seed019.b3nodes.net', 'seed020.b3nodes.net', 'seed021.b3nodes.net', 'seed022.b3nodes.net', 'seed023.b3nodes.net', 'seed024.b3nodes.net', 'seed025.b3nodes.net')
+    seeds =  ('seed001.b3nodes.net', 'seed002.b3nodes.net', 'seed003.b3nodes.net', 'seed004.b3nodes.net', 'seed005.b3nodes.net',
+              'seed006.b3nodes.net', 'seed007.b3nodes.net', 'seed008.b3nodes.net', 'seed008.b3nodes.net', 'seed010.b3nodes.net',
+              'seed011.b3nodes.net', 'seed012.b3nodes.net', 'seed013.b3nodes.net', 'seed014.b3nodes.net', 'seed015.b3nodes.net',
+              'seed016.b3nodes.net', 'seed017.b3nodes.net', 'seed018.b3nodes.net', 'seed019.b3nodes.net', 'seed020.b3nodes.net',
+              'seed021.b3nodes.net', 'seed022.b3nodes.net', 'seed023.b3nodes.net', 'seed024.b3nodes.net', 'seed025.b3nodes.net')
     port = 5647
 
 
