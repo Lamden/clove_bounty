f3eeee6f173a5fca0c8102b729644b2452a422b1
Ancil Cleetus <ancilcleetus@gmail.com>
2018-02-17 17:21:22 +0530

    Add files via upload

---
+++ network/acescoin.py
@@ -0,0 +1,25 @@
+from clove.network.bitcoin import Bitcoin
+
+
+class  Aces(Bitcoin):
+    """
+    Class with all the necessary  Aces (ACES) network information based on
+    https://github.com/aces-coin/AcesCoin/blob/master/src/net.cpp
+    (date of access: 02/17/2018)
+    """
+    name = 'aces'
+    symbols = ('ACES', )
+    seeds =  ('81.4.123.155', '81.4.123.155')
+    port = 21274
+
+
+class  SuperBitcoinTestNet(Aces):
+    """
+    Class with all the necessary  Aces (ACES) network information based on
+    https://github.com/aces-coin/AcesCoin/blob/master/src/net.cpp
+    (date of access: 02/17/2018)
+    """
+    name = 'test-aces'
+    symbols = ('ACES', )
+    seeds =  ()
+    port = 21275
