4530f00df7e877081ac536c1a52319cd1007990e
Ancil Cleetus <ancilcleetus@gmail.com>
2018-02-18 17:51:25 +0530

    Add files via upload

---
+++ network/yenten.py
@@ -0,0 +1,25 @@
+from clove.network.bitcoin import Bitcoin
+
+
+class  Yenten(Bitcoin):
+    """
+    Class with all the necessary  Yenten (YTN) network information based on
+    https://github.com/conan-equal-newone/yenten/blob/master/src/chainparams.cpp
+    (date of access: 02/18/2018)
+    """
+    name = 'yenten'
+    symbols = ('YTN', )
+    seeds =  ('seed.yenten.org')
+    port = 9981
+
+
+class  YentenTestNet(Yenten):
+    """
+    Class with all the necessary  Yenten (YTN) network information based on
+    https://github.com/conan-equal-newone/yenten/blob/master/src/chainparams.cpp
+    (date of access: 02/18/2018)
+    """
+    name = 'test-yenten'
+    symbols = ('YTN', )
+    seeds =  ()
+    port = 19981
