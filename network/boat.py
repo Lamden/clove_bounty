4f5ea30a3d44766d7aeace359a6b57f961ab1a0a
Ancil Cleetus <ancilcleetus@gmail.com>
2018-02-18 17:36:45 +0530

    Add files via upload

--- network/boat.py
+++ network/boat.py
@@ -1,18 +1,25 @@
 from clove.network.bitcoin import Bitcoin
 
 
-class BOAT(Bitcoin):
+class  Boat(Bitcoin):
     """
-    Class with all the necessary BOAT network information based on
-    https://github.com/OBAViJEST/boatcoinfinal/blob/master/src/net.cpp
-    (date of access: 02/14/2018)
+    Class with all the necessary  Boat (BOAT) network information based on
+    https://github.com/cryptofun/blas/blob/master/src/chainparams.cpp
+    (date of access: 02/18/2018)
     """
     name = 'boat'
     symbols = ('BOAT', )
-    seeds = ("seed1.Doubloon.trade",
-             "seed2.Doubloon.trade",
-             "seed3.Doubloon.trade",
-             "seed4.Doubloon.trade")
+    seeds =  ('seed1.Doubloon.trade', 'seed2.Doubloon.trade', 'seed3.Doubloon.trade', 'seed4.Doubloon.trade')
     port = 33827
-	
-# Has no testnet
+
+
+class  BoatTestNet(Boat):
+    """
+    Class with all the necessary  Boat (BOAT) network information based on
+    https://github.com/cryptofun/blas/blob/master/src/chainparams.cpp
+    (date of access: 02/18/2018)
+    """
+    name = 'test-boat'
+    symbols = ('BOAT', )
+    seeds =  ()
+    port = 33728
--- network/ecoin.py
+++ network/ecoin.py
@@ -1,29 +1,25 @@
 from clove.network.bitcoin import Bitcoin
 
 
-class Ecoin(Bitcoin):
+class  Ecoin(Bitcoin):
     """
-    Class with all the necessary ECN network information based on
+    Class with all the necessary  Ecoin (ECN) network information based on
     https://github.com/ecoinclub/ecoin/blob/master/src/net.cpp
-    (date of access: 02/12/2018)
+    (date of access: 02/18/2018)
     """
     name = 'ecoin'
     symbols = ('ECN', )
-    seeds = (
-        '138.68.164.92',
-        'ecnblockchain.com',
-        '72.52.156.4',
-        '72.52.156.87',
-    )
+    seeds =  ('138.68.164.92', 'ecnblockchain.com', '72.52.156.4', '72.52.156.87')
     port = 18741
 
 
-class EcoinTestNet(Ecoin):
+class  EcoinTestNet(Ecoin):
     """
-    Class with all the necessary ECN testing network information based on
+    Class with all the necessary  Ecoin (ECN) network information based on
     https://github.com/ecoinclub/ecoin/blob/master/src/net.cpp
-    (date of access: 02/12/2018)
+    (date of access: 02/18/2018)
     """
     name = 'test-ecoin'
-    seeds = ()
+    symbols = ('ECN', )
+    seeds =  ()
     port = 17778
