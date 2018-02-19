9c8a48c6d252c735cc14be0c67d7157f853095b8
Ancil Cleetus <ancilcleetus@gmail.com>
2018-02-19 14:20:43 +0530

    Add files via upload

---
+++ network/gold_pressed_latinum.py
@@ -0,0 +1,16 @@
+from clove.network.bitcoin import Bitcoin
+
+
+class  GoldPressedLatinum(Bitcoin):
+    """
+    Class with all the necessary  Gold Pressed Latinum (GPL) network information based on
+    https://github.com/scificrypto/Gold-Pressed-Latinum/blob/master/src/net.cpp
+    (date of access: 02/19/2018)
+    """
+    name = 'gold_pressed_latinum'
+    symbols = ('GPL', )
+    seeds =  ('seed.goldpressedlatinum.su')
+    port = 23635
+
+
+# no testnet
