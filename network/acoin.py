ce0fedb55efd51b13279612a04d263dd359fde46
Ancil Cleetus <ancilcleetus@gmail.com>
2018-02-19 12:22:15 +0530

    Add files via upload

--- network/acoin.py
+++ network/acoin.py
@@ -1,16 +1,26 @@
 from clove.network.bitcoin import Bitcoin
 
 
-class Acoin(Bitcoin):
+class  Acoin(Bitcoin):
     """
-    Class with all the necessary Acoin (ACOIN) network information based on
+    Class with all the necessary  Acoin (ACOIN) network information based on
     https://github.com/acoin-project/acoin/blob/master/src/chainparams.cpp
-    (date of access: 02/17/2018)
+    (date of access: 02/19/2018)
     """
     name = 'acoin'
     symbols = ('ACOIN', )
-    seeds = ('seed1.a-coin.info', 'seed2.a-coin.info', 'seed3.a-coin.info', 'seed4.a-coin.info', 
-			 'seed5.a-coin.info', 'seed6.a-coin.info', 'seed7.a-coin.info', 'seed8.a-coin.info')
+    seeds =  ('seed1.a-coin.info', 'seed2.a-coin.info', 'seed3.a-coin.info', 'seed4.a-coin.info',
+				'seed5.a-coin.info', 'seed6.a-coin.info', 'seed7.a-coin.info', 'seed8.a-coin.info')
     port = 17883
 
-# no testnet
+
+class  AcoinTestNet(Acoin):
+    """
+    Class with all the necessary  Acoin (ACOIN) network information based on
+    https://github.com/acoin-project/acoin/blob/master/src/chainparams.cpp
+    (date of access: 02/19/2018)
+    """
+    name = 'test-acoin'
+    symbols = ('ACOIN', )
+    seeds =  ()
+    port = 27883
