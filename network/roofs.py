6691b7cba6bd3a568f85b22cdf5e2e6c4dc11ff2
Ancil Cleetus <ancilcleetus@gmail.com>
2018-02-19 13:07:00 +0530

    Add files via upload

---
+++ network/roofs.py
@@ -0,0 +1,16 @@
+from clove.network.bitcoin import Bitcoin
+
+
+class  Roofs(Bitcoin):
+    """
+    Class with all the necessary  Roofs (ROOFS) network information based on
+    https://github.com/roofsdev/roofs/blob/master/src/net.cpp
+    (date of access: 02/19/2018)
+    """
+    name = 'roofs'
+    symbols = ('ROOFS', )
+    seeds =  ('192.161.48.19')
+    port = 20019
+
+
+# no testnet
