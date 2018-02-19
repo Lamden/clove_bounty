5e24b8b1c63ce75b0fbe25ffad52511055a948b2
Ancil Cleetus <ancilcleetus@gmail.com>
2018-02-19 11:41:30 +0530

    Add files via upload

---
+++ network/2give.py
@@ -0,0 +1,25 @@
+from clove.network.bitcoin import Bitcoin
+
+
+class  TwoGIVE(Bitcoin):
+    """
+    Class with all the necessary  2GIVE (2GIVE) network information based on
+    https://github.com/LittleDuke/2GIVE/blob/master/src/net.cpp
+    (date of access: 02/19/2018)
+    """
+    name = '2give'
+    symbols = ('2GIVE', )
+    seeds =  ('seed2.givecoin.io')
+    port = 6763
+
+
+class  TwoGIVETestNet(TwoGIVE):
+    """
+    Class with all the necessary  2GIVE (2GIVE) network information based on
+    https://github.com/LittleDuke/2GIVE/blob/master/src/net.cpp
+    (date of access: 02/19/2018)
+    """
+    name = 'test-2give'
+    symbols = ('2GIVE', )
+    seeds =  ()
+    port = 16763
